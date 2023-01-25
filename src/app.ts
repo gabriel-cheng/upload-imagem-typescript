fb81e2ea51c14bca9c0acdf12208a953a5678c78
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-01-25 17:51:05 -0300

    feat: adicionando verificação de existência de diretório 'uploads'

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,14 @@ dotenv.config();
 import express from "express";
 import router from "./router/index.router";
 import MongoConnect from "./database/connect.database";
+import path from "path";
+import fs from "fs";
+const directoryExists = fs.existsSync(path.basename(__dirname+"/uploads"));
 const app = express();
+
+if(!directoryExists) {
+    fs.mkdirSync("uploads");
+}
 
 MongoConnect();
 app.use(express.json());
